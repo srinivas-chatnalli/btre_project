f2a136775a8b660045aad1925abcd84095eb4043
srinivas23 <srinivaschat23@gmail.com>
2020-07-17 15:26:08 +0530

    Updated settings file

--- btre/btre/settings.py
+++ btre/btre/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'house-info-app.herokuapp.com']
 
 
 # Application definition

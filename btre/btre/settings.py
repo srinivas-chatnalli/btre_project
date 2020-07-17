c83dc7df653d6939a407b8591ff9527a157aa19b
srinivas23 <srinivaschat23@gmail.com>
2020-07-17 16:25:41 +0530

    Update settings

--- btre/btre/settings.py
+++ btre/btre/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME', '',
+        'NAME': os.environ.get('DB_NAME', ''),
         'USER': os.environ.get('DB_USER', ''),
         'PASSWORD': os.environ.get('DB_PASSWORD', ''),
         'HOST': 'localhost',
